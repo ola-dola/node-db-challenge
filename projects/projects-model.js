45a0c7de7ec607a58c3b0fb89be4d11398d93c80
ola-dola <oredolaolamide@gmail.com>
2020-01-24 13:47:40 +0100

    users can add and retrieve projects list

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -1,22 +1,14 @@
-const db = require('../data/db-config');
+const db = require("../data/db-config");
 
 function find(table) {
-    if (table === 'resources') {
-        return db('resources');
-    } else if ( table === 'projects') {
-        return db('projects');
-    } else {
-        return null;
-    }
+  return db(table);
 }
 
 function insert(table, data) {
-    if (table === 'resources') {
-        return db('resources').insert(data);
-    }
+  return db(table).insert(data);
 }
 
 module.exports = {
-    find,
-    insert
-}
+  find,
+  insert
+};
--- projects/projects-router.js
+++ projects/projects-router.js
@@ -30,4 +30,30 @@ router.post("/resources", (req, res) => {
     });
 });
 
+router.get("/projects", (req, res) => {
+  Helpers.find("projects")
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json("Unable to retrieve at the moment");
+    });
+});
+
+router.post("/projects", (req, res) => {
+  const body = req.body;
+
+  Helpers.insert("projects", body)
+    .then(data => {
+      if (data) {
+        res.status(200).json("New project added");
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json("Unable to add new resource.");
+    });
+});
+
 module.exports = router;
