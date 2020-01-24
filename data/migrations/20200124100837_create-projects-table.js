5928f00c1e4c2dde2e2a41e109bd9c75e6b6c1e9
ola-dola <oredolaolamide@gmail.com>
2020-01-24 10:47:53 +0100

    knex migration schema

--- data/migrations/20200124100837_create-projects-table.js
+++ data/migrations/20200124100837_create-projects-table.js
@@ -1,8 +1,49 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable("projects", tbl => {
+      tbl.increments();
+      tbl
+        .text("Name", 128)
+        .unique()
+        .notNullable();
+      tbl.text("Description");
+      tbl.boolean("Completed").defaultTo(false);
+    })
+    .createTable("resources", tbl => {
+      tbl.increments();
+      tbl
+        .text("Name", 128)
+        .unique()
+        .notNullable();
+      tbl.text("Description");
+    })
+    .createTable("tasks", tbl => {
+      tbl.increments();
+      tbl.text("Description").notNullable();
+      tbl.text("Notes");
+      tbl.boolean("Completed").defaultTo(false);
+      tbl
+        .integer("ProjectID")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("projects")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      tbl
+        .integer("ResourceID")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("resources")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("projects");
 };
