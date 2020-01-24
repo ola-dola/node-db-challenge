df12ad40badc5dcf0356d3a4591d3556a5795a0b
ola-dola <oredolaolamide@gmail.com>
2020-01-24 11:53:52 +0100

    seeded tasks table

--- data/seeds/002-projects.js
+++ data/seeds/002-projects.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       return knex('projects').insert([
         {Name: 'Make Rockect', Description: 'Used to wash clothes 01', Completed: true},
-        {Name: 'Dig holes'},
+        {Name: 'Dig holes', Completed: false},
         {Name: 'Mine Bitumen', Description: 'Used to san disk 03', Completed: false},
-        {Name: 'X-Force Down', Description: 'Used to board card 04'}
+        {Name: 'X-Force Down', Description: 'Used to board card 04', Completed: false}
       ]);
     });
 };
